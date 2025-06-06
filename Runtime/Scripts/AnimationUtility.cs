13138278fc74f99ace64912eba26dbd8489f75fa
Junsang <kimnora070623@gmail.com>
2025-06-06 18:17:00 +0900

    수정 | Null 안정성 강화, 오브젝트 활성화 보정, Fade 메서드 내 Color 최적화, 스케일 애니메이션 분리

--- Runtime/Scripts/AnimationUtility.cs
+++ Runtime/Scripts/AnimationUtility.cs
@@ -22,6 +22,10 @@ public static class AnimationUtility
     #region UI Tween Helpers
     private static void StartManagedCoroutine(MonoBehaviour runner, object key, IEnumerator coroutine)
     {
+        if (runner == null) return;
+
+        if (key == null) return;
+        
         if (RunningCoroutines.TryGetValue(key, out var existing))
         {
             runner.StopCoroutine(existing);
@@ -38,7 +42,11 @@ public static class AnimationUtility
     }
     
     private static IEnumerator Animate(
-        float duration, float delay, Action<float> onUpdate, Action onPlay = null, Action onComplete = null,
+        float duration, 
+        float delay, 
+        Action<float> onUpdate = null,
+        Action onPlay = null, 
+        Action onComplete = null,
         Func<float, float> easingFunc = null)
     {
         if (delay > 0f)
@@ -63,52 +71,91 @@ public static class AnimationUtility
     private static IEnumerator Move(RectTransform rect, float duration, float delay, Vector2 toMove,
         Action onPlay, Action onComplete)
     {
-        var startPosition = rect.anchoredPosition;
-        return Animate(duration, delay,
+        if (rect == null) yield break;
+        if(!rect.gameObject.activeInHierarchy) rect.gameObject.SetActive(true);
+        
+        Vector2 startPosition = rect.anchoredPosition;
+        yield return Animate(duration, delay,
             t => rect.anchoredPosition = Vector2.Lerp(startPosition, toMove, t),
             onPlay, onComplete, Easing.EaseOutQuad);
     }
 
-    private static IEnumerator FadeIn(Graphic graphic, float duration, float delay, Color color, Action onPlay,
+    private static IEnumerator Fade(Graphic graphic, Color color, float fromAlpha, float toAlpha, float duration, float delay, Action onPlay,
         Action onComplete)
     {
-        return Animate(duration, delay,
-            t => graphic.color = new Color(color.r, color.g, color.b, t),
-            onPlay, onComplete, Easing.EaseOutQuad);
-    }
-    
-    private static IEnumerator FadeOut(Graphic graphic, float duration, float delay, Color color, Action onPlay,
-        Action onComplete)
-    {
-        return Animate(duration, delay,
-            t => graphic.color = new Color(color.r, color.g, color.b, 1 - t),
-            onPlay, onComplete, Easing.EaseOutQuad);
+        if(graphic == null) yield break;
+        if(!graphic.gameObject.activeInHierarchy) graphic.gameObject.SetActive(true);
+
+        Color tempColor = color;
+        tempColor.a = fromAlpha;
+        yield return Animate(duration, delay,
+            t =>
+            {
+                float currentAlpha = Mathf.Lerp(fromAlpha, toAlpha, t);
+                graphic.color = new Color(tempColor.r, tempColor.g, tempColor.b, currentAlpha);
+            },
+            onPlay, 
+            onComplete, 
+            Easing.EaseOutQuad);
     }
 
     private static IEnumerator ImageSlide(Image image, float duration, float delay, float start, float end,
         int fillOrigin, Action onPlay, Action onComplete)
     {
-        image.fillOrigin = fillOrigin;
+        if(image == null) yield break;
+        if(!image.gameObject.activeInHierarchy) image.gameObject.SetActive(true);
         
-        return Animate(duration, delay,
+        image.fillOrigin = fillOrigin;
+        yield return Animate(duration, delay,
             t => image.fillAmount = Mathf.Lerp(start, end, t),
-            onPlay, onComplete, Easing.EaseOutQuad);
+            onPlay, 
+            onComplete, 
+            Easing.EaseOutQuad);
     }
 
     private static IEnumerator ScaleX(GameObject obj, float from, float to, float duration, float delay,
         Action onPlay, Action onComplete)
     {
-        return Animate(duration, delay,
-            t => obj.transform.localScale = new Vector3(Mathf.Lerp(from, to, t), obj.transform.localScale.y, obj.transform.localScale.z),
-            onPlay, onComplete);
-    }
+        if(obj == null) yield break;
+        if(!obj.activeInHierarchy) obj.gameObject.SetActive(true);
 
-    private static IEnumerator ScaleY(GameObject obj, float fromSize, float toSize, float duration, float delay,
+        yield return Animate(duration, delay,
+            t =>
+            {
+                Vector3 currentScale = obj.transform.localScale;
+                currentScale.x = Mathf.Lerp(from, to, t);
+                obj.transform.localScale = currentScale;
+            },
+            onPlay, 
+            onComplete,
+            Easing.EaseOutQuad);
+    }
+    
+    private static IEnumerator ScaleY(GameObject obj, float from, float to, float duration, float delay,
         Action onPlay, Action onComplete)
     {
-        return Animate(duration, delay,
-            t => obj.transform.localScale = new Vector3(obj.transform.localScale.x, Mathf.Lerp(fromSize, toSize, t), obj.transform.localScale.z),
-            onPlay, onComplete);
+        if(obj == null) yield break;
+        if(!obj.activeInHierarchy) obj.gameObject.SetActive(true);
+
+        yield return Animate(duration, delay,
+            t =>
+            {
+                Vector3 currentScale = obj.transform.localScale;
+                currentScale.y = Mathf.Lerp(from, to, t);
+                obj.transform.localScale = currentScale;
+            },
+            onPlay, 
+            onComplete,
+            Easing.EaseOutQuad);
+    }
+
+    private static IEnumerator ScaleAxis(GameObject obj, float from, float to, float duration, float delay,
+        Action onPlay, Action onComplete, bool isXAxis)
+    {
+        
+        return isXAxis ? 
+            ScaleX(obj, from, to, duration, delay, onPlay, onComplete) :
+            ScaleY(obj, from, to, duration, delay, onPlay, onComplete);
     }
     #endregion
 
@@ -116,11 +163,12 @@ public static class AnimationUtility
     /// <summary>
     /// 특정 애니메이션 정지
     /// </summary>
-    /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
-    /// <param name="rect">오브젝트</param>
     /// <typeparam name="T">데이터 형식</typeparam>
+    /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
+    /// <param name="rect">애니메이션 대상 오브젝트</param>
     public static void StopAnimation<T>(MonoBehaviour runner, T rect)
     {
+        if(runner == null) return;
         if (RunningCoroutines.TryGetValue(rect, out var coroutine))
         {
             runner.StopCoroutine(coroutine);
@@ -131,12 +179,12 @@ public static class AnimationUtility
     /// 이동 애니메이션
     /// </summary>
     /// <param name="runner">MonoBehavior를 상속한 클래스</param>
-    /// <param name="rect">오브젝트</param>
-    /// <param name="duration">지속시간</param>
-    /// <param name="delay">지연시간</param>
-    /// <param name="toMove">이동할 위치</param>
-    /// <param name="onPlay">시작했을 때 실행할 행동</param>
-    /// <param name="onComplete">끝났을 때 실행할 행동</param>
+    /// <param name="rect">대상 RectTransform</param>
+    /// <param name="duration">애니메이션 지속시간</param>
+    /// <param name="delay">애니메이션 지연시간</param>
+    /// <param name="toMove">이동할 최종 위치</param>
+    /// <param name="onPlay">애니메이션 시작 시 실행할 행동</param>
+    /// <param name="onComplete">애니메이션 완료 시 실행할 행동</param>
     public static void MoveAnimation(MonoBehaviour runner, RectTransform rect, float duration, float delay, Vector2 toMove,
         Action onPlay = null, Action onComplete = null)
     {
@@ -144,49 +192,35 @@ public static class AnimationUtility
     }
 
     /// <summary>
-    /// 페이드 인 애니메이션
+    /// 페이드 애니메이션
     /// </summary>
     /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
-    /// <param name="graphic">이미지 또는 텍스트</param>
-    /// <param name="duration">지속시간</param>
-    /// <param name="delay">지연시간</param>
-    /// <param name="color">색상</param>
-    /// <param name="onPlay">시작했을 때 실행할 행동</param>
-    /// <param name="onComplete">끝났을 때 실행할 행동</param>
-    public static void FadeInAnimation(MonoBehaviour runner, Graphic graphic, float duration, float delay, Color color,
+    /// <param name="graphic">대상 이미지 또는 텍스트</param>
+    /// <param name="color">기본 색상</param>
+    /// <param name="fromAlpha">시작 알파 값</param>
+    /// <param name="toAlpha">종료 알파 값</param>
+    /// <param name="duration">애니메이션 지속시간</param>
+    /// <param name="delay">애니메이션 지연시간</param>
+    /// <param name="onPlay">애니메이션 시작 시 실행할 행동</param>
+    /// <param name="onComplete">애니메이션 완료 시 실행할 행동</param>
+    public static void FadeAnimation(MonoBehaviour runner, Graphic graphic, Color color, float fromAlpha, float toAlpha, float duration, float delay,
         Action onPlay, Action onComplete)
     {
-        StartManagedCoroutine(runner, graphic, FadeIn(graphic, duration, delay, color, onPlay, onComplete));
+        StartManagedCoroutine(runner, graphic, Fade(graphic, color, fromAlpha, toAlpha, duration, delay, onPlay, onComplete));
     }
 
     /// <summary>
-    /// 페이드 아웃 애니메이션
+    /// 슬라이드 애니메이션 (fillAmount 조절)
     /// </summary>
     /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
-    /// <param name="graphic">이미지 또는 텍스트</param>
-    /// <param name="duration">지속시간</param>
-    /// <param name="delay">지연시간</param>
-    /// <param name="color">색상</param>
-    /// <param name="onPlay">시작했을 때 실행할 행동</param>
-    /// <param name="onComplete">끝났을 때 실행할 행동</param>
-    public static void FadeOutAnimation(MonoBehaviour runner, Graphic graphic, float duration, float delay, Color color,
-        Action onPlay, Action onComplete)
-    {
-        StartManagedCoroutine(runner, graphic, FadeOut(graphic, duration, delay, color, onPlay, onComplete));
-    }
-
-    /// <summary>
-    /// 슬라이드 애니메이션
-    /// </summary>
-    /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
-    /// <param name="image">이미지</param>
-    /// <param name="duration">지속시간</param>
-    /// <param name="delay">지연시간</param>
-    /// <param name="fillOrigin">이동 방향</param>
-    /// <param name="onPlay">시작했을 때 실행할 행동</param>
-    /// <param name="onComplete">끝났을 때 실행할 행동</param>
-    /// <param name="start">시작 값</param>
-    /// <param name="end">끝 값</param>
+    /// <param name="image">대상 이미지</param>
+    /// <param name="duration">애니메이션 지속시간</param>
+    /// <param name="delay">애니메이션 지연시간</param>
+    /// <param name="start">시작 fillAmount 값</param>
+    /// <param name="end">끝 fillAmount 값</param>
+    /// <param name="fillOrigin">이미지 채우기 시작 방향</param>
+    /// <param name="onPlay">애니메이션 시작 시 실행할 행동</param>
+    /// <param name="onComplete">애니메이션 완료 시 실행할 행동</param>
     public static void SlideAnimation(MonoBehaviour runner, Image image, float duration, float delay, float start,
         float end, int fillOrigin, Action onPlay, Action onComplete)
     {
@@ -194,37 +228,21 @@ public static class AnimationUtility
     }
 
     /// <summary>
-    /// 스케일X 애니메이션
+    /// 스케일 애니메이션 (X축 또는 Y축)
     /// </summary>
     /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
-    /// <param name="obj">오브젝트</param>
-    /// <param name="fromSize">몇에서</param>
-    /// <param name="toSize">몇으로</param>
-    /// <param name="duration">지속시간</param>
-    /// <param name="delay">지연시간</param>
-    /// <param name="onPlay">시작했을 때 실행할 행동</param>
-    /// <param name="onComplete">끝났을 때 실행할 행동</param>
-    public static void ScaleXAnimation(MonoBehaviour runner, GameObject obj, float fromSize, float toSize,
-        float duration, float delay, Action onPlay, Action onComplete)
+    /// <param name="obj">대상 GameObject</param>
+    /// <param name="from">시작 scale 값</param>
+    /// <param name="to">종료 scale 값</param>
+    /// <param name="duration">애니메이션 지속시간</param>
+    /// <param name="delay">애니메이션 지연시간</param>
+    /// <param name="onPlay">애니메이션 시작 시 실행할 행동</param>
+    /// <param name="onComplete">애니메이션 완료 시 실행할 행동</param>
+    /// <param name="isXAxis">true이면 X축, 아니면 Y축 애니메이션 수행</param>
+    public static void ScaleAxisAnimation(MonoBehaviour runner, GameObject obj, float from, float to,
+        float duration, float delay, Action onPlay, Action onComplete, bool isXAxis = false)
     {
-        StartManagedCoroutine(runner, obj, ScaleX(obj, fromSize, toSize, duration, delay, onPlay, onComplete));
-    }
-
-    /// <summary>
-    /// 스케일Y 애니메이션
-    /// </summary>
-    /// <param name="runner">MonoBehaviour를 상속한 클래스</param>
-    /// <param name="obj">오브젝트</param>
-    /// <param name="fromSize">몇에서</param>
-    /// <param name="toSize">몇으로</param>
-    /// <param name="duration">지속시간</param>
-    /// <param name="delay">지연시간</param>
-    /// <param name="onPlay">시작했을 때 실행할 행동</param>
-    /// <param name="onComplete">끝났을 때 실행할 행동</param>
-    public static void ScaleYAnimation(MonoBehaviour runner, GameObject obj, float fromSize, float toSize,
-        float duration, float delay, Action onPlay, Action onComplete)
-    {
-        StartManagedCoroutine(runner, obj, ScaleY(obj, fromSize, toSize, duration, delay, onPlay, onComplete));
+        StartManagedCoroutine(runner, obj, ScaleAxis(obj, from, to, duration, delay, onPlay, onComplete, isXAxis));
     }
     #endregion
 }
